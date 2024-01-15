be7a58a35670db196eaa6ba487e2a0116dafc35b
br0nke <mantulisr@gmail.com>
2024-01-15 07:51:28 +0000

    Mant prideta classe

--- cards_game.py
+++ cards_game.py
@@ -1,13 +1,16 @@
 #Kort kaladė
 #Korta: objektas
--- #rank (2-9, T, J, Q, K, A)
--- #suit (spades, clubs, hearts, diamonds)
--- #sign (suit + rank)
--- #weight
+#-- #rank (2-9, T, J, Q, K, A)
+#-- #suit (spades, clubs, hearts, diamonds)
+#-- #sign (suit + rank)
+#-- #weight
 #Kortų kaladė
--- #cards - sąrašas kortų
--- #shuffle
--- #take from top
--- #take from bottom
--- #take random
+#-- #cards - sąrašas kortų
+#-- #shuffle
+#-- #take from top
+#-- #take from bottom
+#-- #take random
 #mastom apie žaidimą
+
+
+class Cards():
