2303776ee193ede197e7b8f450b9ad3ea28778e7
aivarasmink <aivarasmink@gmail.com>
2024-01-15 13:01:58 +0200

    sukuriau noutraukas kortu

--- cards_game.py
+++ cards_game.py
@@ -1,6 +1,138 @@
-import random
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+# Create a deck of cards for testing
+deck = [
+    Card('2', 'Hearts'), Card('3', 'Diamonds'), Card('4', 'Clubs'), Card('5', 'Spades'),
+    Card('6', 'Hearts'), Card('7', 'Diamonds'), Card('8', 'Clubs'), Card('9', 'Spades'),
+    Card('10', 'Hearts'), Card('J', 'Diamonds'), Card('Q', 'Clubs'), Card('K', 'Spades'),
+    Card('A', 'Hearts')
+]
 
 
+
+
+
+
+
+# Function to print the cards
+def print_cards(cards, hidden):
+         
+    s = ""
+    for card in cards:
+        s = s + "\t ________________"
+    if hidden:
+        s += "\t ________________"
+    print(s)
+ 
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|                |"    
+    print(s)
+ 
+    s = ""
+    for card in cards:
+        if card.value == '10':
+            s = s + "\t|  {}            |".format(card.value)
+        else:
+            s = s + "\t|  {}             |".format(card.value)  
+    if hidden:
+        s += "\t|                |"    
+    print(s)
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|      * *       |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|    *     *     |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|   *       *    |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|   *       *    |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|       {}        |".format(card.suit)
+    if hidden:
+        s += "\t|          *     |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|         *      |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|        *       |"
+    print(s)
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|                |"
+    print(s)
+ 
+    s = ""
+    for card in cards:
+        s = s + "\t|                |"
+    if hidden:
+        s += "\t|                |"
+    print(s)    
+ 
+    s = ""
+    for card in cards:
+        if card.value == '10':
+            s = s + "\t|            {}  |".format(card.value)
+        else:
+            s = s + "\t|            {}   |".format(card.value)
+    if hidden:
+        s += "\t|        *       |"        
+    print(s)    
+         
+    s = ""
+    for card in cards:
+        s = s + "\t|________________|"
+    if hidden:
+        s += "\t|________________|"
+    print(s)        
+ 
+    print()
+
+
+# Print the cards with hidden status set to False
+print_cards(deck, hidden=False)
+
+# Print the cards with hidden status set to True
+print_cards(deck, hidden=True)
 
 
 
